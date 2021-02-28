7cfeafc025c10d381034de35b9f04fc42a88ca54
MAC <mac@MACs-MacBook-Air.local>
2021-02-28 22:14:27 +0100

    account

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -21,7 +21,7 @@ class Controller extends BaseController
         $beautymail->send('mails.message', ['data' => $data], function($message) use ($user, $sub)
         {
             $message
-                ->from('noreply@cryptoassets.com')
+                ->from(env('MAIL_FROM_ADDRESS'))
                 ->to($user->email, $user->username)
                 ->subject($sub);
         });
