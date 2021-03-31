10a99d7ba29d3c89c1131c19100e588670cd182b
MAC <mac@MACs-MacBook-Air.local>
2021-03-31 12:00:57 +0100

    modified percent

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
     }
 
     public function uploadId(){
+        return redirect()->route('backend.account.upload.id');
         return view('backend.profile.uploadId');
     }
 
