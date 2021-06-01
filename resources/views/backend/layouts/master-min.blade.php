0f87b794d76ee32f329620658d07c71423213fab
MAC <mac@MACs-MacBook-Air.local>
2021-06-01 23:49:34 +0100

    fixed pages

--- resources/views/backend/layouts/master-min.blade.php
+++ resources/views/backend/layouts/master-min.blade.php
@@ -53,14 +53,6 @@
 
 <body id="dashboard">
 
-<div id="preloader">
-    <div class="sk-three-bounce">
-        <div class="sk-child sk-bounce1"></div>
-        <div class="sk-child sk-bounce2"></div>
-        <div class="sk-child sk-bounce3"></div>
-    </div>
-</div>
-
 <div id="main-wrapper">
 
     <div class="header dashboard">
