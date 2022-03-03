ddcfcb7bac4370e75e7fd20aa660842f0bf9fb36
Your Name <you@example.com>
2022-03-03 21:16:02 +0100

    update

--- app/Http/Controllers/admin/UsersController.php
+++ app/Http/Controllers/admin/UsersController.php
@@ -29,7 +29,7 @@ class UsersController extends Controller
     }
     public function approvedUsers(){
         $title = 'Approved Users';
-        $users = User::whereRoleIs('users')->where('is_active', true)->latest()->get();
+        $users = User::whereRoleIs('users')->where('is_active', 1)->latest()->get();
         return view('admin.users.index', compact('users','title'));
     }
 
