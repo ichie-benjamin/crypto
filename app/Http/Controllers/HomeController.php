2f7f6072ba85cfd206788c8fc4a7626cc26c12d1
Your Name <you@example.com>
2022-03-26 23:47:45 +0100

    s3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -97,8 +97,7 @@ class HomeController extends Controller
         return view('home', compact('traders','all_plans'));
     }
 
-    public function about()
-    {
+    public function about(){
         return view('about1');
     }
     public function withdrawal()
