1d995af08456597474272f7a3e029be34c370821
Your Name <you@example.com>
2022-03-27 08:07:52 +0100

    added s3

--- app/traits/UploadTrait.php
+++ app/traits/UploadTrait.php
@@ -3,16 +3,20 @@
 
 namespace App\traits;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 trait UploadTrait
 {
     public function uploadOne(UploadedFile $uploadedFile, $folder = null, $disk = 'public', $filename = null)
     {
-        $name = !is_null($filename) ? $filename : Str::random(25);
-
-        $file = $uploadedFile->storeAs($folder, $name.'.'.$uploadedFile->getClientOriginalExtension(), $disk);
-
-        return $file;
+//        $name = !is_null($filename) ? $filename : Str::random(25);
+//
+//        $file = $uploadedFile->storeAs($folder, $name.'.'.$uploadedFile->getClientOriginalExtension(), $disk);
+//
+//        return $file;
+        $path = Storage::disk('s3')->put('images', $uploadedFile);
+        $path = Storage::disk('s3')->url($path);
+        return $path;
     }
 }
